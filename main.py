21c6ed60084814beb54139172cee6c5afb2992ac
SidMurthy97 <murthy.siddhant@googlemail.com>
2020-07-16 17:40:25 +0100

    basic elo update working

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 
 
 
+def elo_ranking(A,B):
+
+    k = 32 # K factor
+
+    #hardcode outcome for now, A wins against B 
+    Sa = 1
+    Sb = 0
+
+    Ea = 1/(1 + 10**((B - A )/400)) #expected score for A 
+    Eb = 1/(1 + 10**((A - B)/400)) #expected score for B
+
+    A = A + k * (Sa - Ea)
+    B = B + k * (Sb - Eb)
+    return A,B
 
 
 
 if __name__ == '__main__':
-    print("test")
+    print("Starting..")
+    a,b = elo_ranking(90,110)
+    print(a,b)
